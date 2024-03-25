c5725f9614c8d3b60c6cb3555e7eb6849fa42fdf
nithi <g.d.nithi5@gmail.com>
2024-03-25 19:33:53 +0530

    Service and Template update

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -18,8 +18,8 @@ const BookingForm = () => {
     alert('You will be contacted very soon! Please feel free to call us.');
     // Reset form fields after successful submission
 
-    const serviceId = "service_js8wofc";
-    const templateId = "template_gv3blsj";
+    const serviceId = "service_466cc2q";
+    const templateId = "template_l4ydhm8";
 
     emailjs.send(serviceId, templateId, {
       'name': name,
